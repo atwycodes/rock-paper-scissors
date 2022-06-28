0bb9cbb53aad28132ce2ad73bb01303989ed0d04
Andrew Tan <andrewwytan@gmail.com>
2022-06-29 00:01:57 +0800

    added playGame() function, to continue with criteria of game

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ function computerPlay() {
     // this function will randomly return either "Rock", "Paper" or "Scissors".
 
     const random = Math.floor(Math.random() * gameChoices.length);
-
+    
     return gameChoices[random]
 }
 
+const computerSelection = computerPlay()
+
+function playGame(playerSelection, computerSelection) {
+    let playerSelection = prompt("Enter your chosen weapon!")
+    if playerSelection == computerSelection {
+      // to continue criteria
+    }
+}
