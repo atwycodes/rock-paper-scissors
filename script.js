b024237e7e4861fc2f15c2742cc24ca081496324
Andrew Tan <andrewwytan@gmail.com>
2022-07-03 00:06:09 +0800

    added game() function which runs game 5 times, reports result at the end of each round and reports final score.

--- script.js
+++ script.js
@@ -10,20 +10,35 @@ function computerPlay() {
 }
 
 // converting to lowercase to remove need for case-sensitive input
-let computerSelection = computerPlay().toLowerCase(); 
-let playerSelection = String(prompt("Enter your chosen weapon!")).toLowerCase(); 
 
-function playRound(playerSelection,computerSelection) {
-  computerPlay()
-  if (playerSelection == computerSelection) {
-    return `Tie Game. You selected ${playerSelection} & Your Opponent selected ${computerSelection}`
+// if variable is a global function, it must be called as function(globalvar1, globalvar2)
+let playerScore = 0;
+let computerScore =0;
 
-  } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
-    return `You Lost... ${computerSelection} beats ${playerSelection}` 
-
-  } else {
-    return `You Won! ${playerSelection} beats ${computerSelection}` 
-  }
+function playRound() {
+    let computerSelection = computerPlay().toLowerCase(); 
+    let playerSelection = String(prompt("Enter your chosen weapon!")).toLowerCase(); 
+    if (playerSelection == computerSelection) {
+    return result = `Tie Game. You selected ${playerSelection} & Your Opponent selected ${computerSelection}`
+    // losing logic
+    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+        computerScore++
+        return result =  `You Lost... ${computerSelection} beats ${playerSelection}` 
+    // winning logic
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore++
+        return result =  `You Won! ${playerSelection} beats ${computerSelection}` 
+    } else {
+        return result =  `${playerSelection} is invalid. Please call the function again.`
+    }
 }
 
-
+// game() runs the playRound() function 5 times, keeps score and reports a winner or loser at the end.
+function game () {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log(`Round ${i+1} results:`)
+        console.log(result);
+     }
+     return console.log(`PLAYER ${playerScore} vs ${computerScore} COMPUTER`)
+}
