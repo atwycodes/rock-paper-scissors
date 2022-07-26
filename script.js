14a0bb0e1658732e562d102bf8f3c576cd0e89ed
Andrew Tan <andrewwytan@gmail.com>
2022-07-26 21:56:22 +0800

    changed computerPlay function to use object-indexing for less verbose code - thanks Ray!

--- script.js
+++ script.js
@@ -14,35 +14,44 @@ let playerScore = 0;
 let computerScore =0;
 
 function computerPlay() {
-    // this function will randomly return either "Rock", "Paper" or "Scissors".
-
+    // this function will randomly return either "Rock", "Paper" or "Scissors"
     const random = Math.floor(Math.random() * options.length);
-    
     return options[random]
 }
-
 
 function playRound() {
     let computerSelection = computerPlay().toLowerCase(); 
     let playerSelection = String(prompt("Enter your chosen weapon!")).toLowerCase(); 
+    
+    const losingConditions = {
+        rock: "paper",
+        paper: "scissors",
+        scissors: "rock",
+    }
+    
+    const winningConditions = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper",
+    }
+    
     if (playerSelection == computerSelection) {
-        return result = `Tie Game. You selected ${playerSelection} & Your Opponent selected ${computerSelection}`
-    // losing logic
-    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
-        computerScore++
-        return result =  `You Lost... ${computerSelection} beats ${playerSelection}` 
-    // winning logic
-    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        playerScore++
-        return result =  `You Won! ${playerSelection} beats ${computerSelection}` 
+        return result = `Tie Game. You selected ${playerSelection} & Your Opponent selected ${computerSelection}`;
+
+    } else if (losingConditions[playerSelection] === computerSelection) {
+        // player loses
+        computerScore++;
+        return result =  `You Lost... ${computerSelection} beats ${playerSelection}`;
+
+    } else if (winningConditions[playerSelection] === computerSelection) {
+        // player wins
+        playerScore++;
+        return result =  `You Won! ${playerSelection} beats ${computerSelection}` ;
+
     } else {
-        return result =  `${playerSelection} is invalid. Please call the function again.`
+        return result =  `${playerSelection} is invalid. Please call the function again.`;
     }
 }
-
-
-
-
 // game() runs the playRound() function 5 times, keeps score and reports a winner or loser at the end.
 // function game () {
 //     for (let i = 0; i < 5; i++) {
